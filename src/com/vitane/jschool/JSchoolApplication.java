f6a71e852b35bcb136348278b6dd2af9018feddd
vitaly <vitaly.nec@gmail.com>
2019-08-06 17:57:14 +0700

    init main application

---
+++ src/com/vitane/jschool/JSchoolApplication.java
@@ -0,0 +1,11 @@
+package com.vitane.jschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSchoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JSchoolApplication.class, args);
+    }
+}
